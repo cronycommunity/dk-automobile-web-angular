2b82d395dd3656f7dfc2c7ddc1ae3cf2678a6e87
kale1st <devmath1st@hotmail.com>
2022-05-28 18:05:17 +0200

    getting marks works well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarmodelsComponent } from './carmodels/carmodels.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CarmodelsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/cars.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarsService {
+
+  constructor( private httpClient: HttpClient) { }
+  
+  url ="https://listing-creation.api.autoscout24.com/makes?makeId=13&culture=de-DE";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'responseType': 'text' 
+    }),
+  };
+
+  getCarMarks(markNumber:number): Observable<any> {
+    return this.httpClient.get(`https://listing-creation.api.autoscout24.com/makes?makeId=${markNumber}&culture=de-DE`, this.httpOptions);
+  }
+}
