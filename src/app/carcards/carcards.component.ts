d28dbd465ce97e389c9486d9e69640a22a952707
azizkale <azizkale@hotmail.com>
2022-06-13 09:55:43 +0200

    data was called but not wortking

--- src/app/carcards/carcards.component.ts
+++ src/app/carcards/carcards.component.ts
@@ -10,14 +10,18 @@ export class CarcardsComponent implements OnInit {
 
   constructor(private carservice: CarsService) { }
 
+  constData:any;
+
   ngOnInit(): void {
-    this.getConstCarPhotos()
+    this.getConstCarPhotos();
   }
 
   getConstCarPhotos(){
-this.carservice.getConstData().subscribe(data =>{
-  console.log(data)
-})
+    this.carservice.getConstData().subscribe(data =>{
+      this.constData = data;
+      console.log(this.constData)
+
+    })
   }
 
 }
